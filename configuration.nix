a5b5d531b826c15c35f4e52babb576ec7920799b
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2026-01-15 19:37:12 +0530

    24 current  2025-09-05 09:49:56  25.05.808020.c5f08b62ed75  6.16.0                          *

--- configuration.nix
+++ configuration.nix
@@ -179,6 +179,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     libreoffice
+    wine64
     vscodium
     hyprshot
     thunderbird
